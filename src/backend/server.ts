59196169764a56171feee8a1757d938784bbc8e6
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-04-08 16:37:30 +0330

    update server.ts

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,60 +3,57 @@ import { Hono } from "hono"
 import { db } from "./db"
 import { TodoTable } from "./db/schema"
 
+// init app
 export const app = new Hono().basePath("/api")
 
 // get all todos
-app.get("/todos", async (c) => {
-  const { filter } = c.req.query()
+app.get("/todos", async ({ req, json }) => {
+  const { filter } = req.query()
   switch (filter) {
     case "all":
-      return c.json(
+      return json(
         await db.select().from(TodoTable).orderBy(desc(TodoTable.createdAt))
       )
     case "done":
-      return c.json(
+      return json(
         await db.select().from(TodoTable).where(eq(TodoTable.done, true))
       )
     case "undone":
-      return c.json(
+      return json(
         await db.select().from(TodoTable).where(eq(TodoTable.done, false))
       )
   }
 })
-
 // create new todo
-app.post("/todos", async (c) => {
-  const { text } = await c.req.json()
+app.post("/todos", async ({ req, json }) => {
+  const { text } = await req.json()
   const newTodo = await db
     .insert(TodoTable)
     .values({ text })
     .returning({ id: TodoTable.id, text: TodoTable.text, done: TodoTable.done })
-  return c.json(newTodo[0])
+  return json(newTodo[0])
 })
-
 // delete todo by id
-app.delete("/todos/:id", async (c) => {
-  const id = await c.req.param("id")
+app.delete("/todos/:id", async ({ req, json }) => {
+  const id = await req.param("id")
   const deleted = await db
     .delete(TodoTable)
     .where(eq(TodoTable.id, parseInt(id)))
-  return c.json({ msg: "todo has been deleted" })
+  return json({ msg: "todo has been deleted" })
 })
-
 // clear all todos
-app.delete("/todos", async (c) => {
+app.delete("/todos", async ({ json }) => {
   const deleted = await db.delete(TodoTable)
-  return c.json({ msg: "todos has been cleared" })
+  return json({ msg: "todos has been cleared" })
 })
-
 // done todo
-app.patch("/todos/:id", async (c) => {
-  const id = await c.req.param("id")
-  const { done } = await c.req.json()
+app.patch("/todos/:id", async ({ req, json }) => {
+  const id = await req.param("id")
+  const { done } = await req.json()
   const updated = await db
     .update(TodoTable)
     .set({ done })
     .where(eq(TodoTable.id, parseInt(id)))
     .returning({ id: TodoTable.id, text: TodoTable.text, done: TodoTable.done })
-  return c.json({ msg: "todo was updated" })
+  return json({ msg: "todo was updated" })
 })
