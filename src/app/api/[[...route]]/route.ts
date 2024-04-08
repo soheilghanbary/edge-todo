0d3f1223af87e15185c299440e6c1481fd16a2ee
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-04-08 15:53:41 +0330

    update route api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,18 @@
+// import { app } from "@/server"
 import { app } from "@/backend/server"
 import { handle } from "hono/vercel"
 
 export const runtime = "edge"
-
+// Hono
 export const GET = handle(app)
 export const POST = handle(app)
 export const DELETE = handle(app)
 export const PUT = handle(app)
 export const PATCH = handle(app)
+
+// Elysia
+// export const GET = app.handle
+// export const POST = app.handle
+// export const DELETE = app.handle
+// export const PUT = app.handle
+// export const PATCH = app.handle
