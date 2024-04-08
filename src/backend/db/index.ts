adc013eedd3a6a402d86b789b4b62f62e5d6879d
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-04-08 09:08:37 +0330

    update drizzle config

--- src/backend/db/index.ts
+++ src/backend/db/index.ts
@@ -1,5 +1,7 @@
-import { Pool } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-serverless"
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
-export const db = drizzle(pool)
+// connection by url
+const sql = neon(process.env.DATABASE_URL!)
+// database
+export const db = drizzle(sql)
