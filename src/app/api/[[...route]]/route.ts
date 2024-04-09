12dceb5ffa73ef09dc948de51de47af68e1f75bf
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-04-09 20:16:00 +0330

    update more

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,4 +1,3 @@
-// import { app } from "@/server"
 import { app } from "@/backend/server"
 import { handle } from "hono/vercel"
 
@@ -9,10 +8,3 @@ export const POST = handle(app)
 export const DELETE = handle(app)
 export const PUT = handle(app)
 export const PATCH = handle(app)
-
-// Elysia
-// export const GET = app.handle
-// export const POST = app.handle
-// export const DELETE = app.handle
-// export const PUT = app.handle
-// export const PATCH = app.handle
